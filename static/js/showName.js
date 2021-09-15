1ad6df8bdf909665e23724cb454569e3db9579ab
jjubbu <hogugugut4102.off@gmail.com>
2021-09-15 23:15:05 +0900

    닉네임 랜덤 결과창

---
+++ static/js/showName.js
@@ -0,0 +1,23 @@
+$(document).ready(function(){
+    $('#saveName').hide();
+    let resultName = $('.NameResult').text();
+
+//저장하기
+$('.saveToMyPage').click(function(){
+    $('.saveAlert').text(resultName);
+    $('#saveName').css('display','flex');
+});
+
+//저장 알림창 닫기
+$('#box .close').click(function(){
+    $('#saveName').hide();
+    location.href = "get_name.html";
+});
+
+
+
+
+
+
+
+});
