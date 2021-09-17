022339036bcc3dd4bd8f70cd05cecac50b15220c
Wooseok Han <wswj9608@naver.com>
2021-09-17 15:52:13 +0900

    추가 - ID 중복 체크 기능

--- app.py
+++ app.py
@@ -40,18 +40,18 @@ def sign_in():
 
     username_receive = request.form['username_give']
     password_receive = request.form['password_give']
-    print(username_receive)
-    print(password_receive)
+
+
     pw_hash = hashlib.sha256(password_receive.encode('utf-8')).hexdigest()
     result = db.usersdb.find_one({'username': username_receive, 'password': pw_hash})
-    print(result)
+
     if result is not None:
         payload = {
          'id': username_receive,
          'exp': datetime.utcnow() + timedelta(seconds=60)  # 로그인 24시간 유지
         }
         token = jwt.encode(payload, SECRET_KEY, algorithm='HS256').decode('utf-8')
-        print(token + '하나')
+
         return jsonify({'result': 'success', 'token': token})
     # 찾지 못하면
     else:
@@ -72,6 +72,13 @@ def sign_up():
 
     return jsonify({'result': 'success'})
 
+@app.route('/sign_up/check_dup', methods=['POST'])
+def check_dup():
+    username_receive = request.form['username_give']
+    exists = bool(db.usersdb.find_one({"username": username_receive}))
+    # print(value_receive, type_receive, exists)
+    return jsonify({'result': 'success', 'exists': exists})
+
 
 # html 연결하기 끝
 
@@ -91,7 +98,7 @@ def save_nick():
     nick_receive = request.form['nick_give']
     cookieId_receive = request.form['cookieId_give']
 
-    id_list = list(db.usersdb.find({'cookieId': cookieId_receive}, {'_id': False}))
+    id_list = list(db.mynick.find({'cookieId': cookieId_receive}, {'_id': False}))
 
     id_count = len(id_list)
 
@@ -106,7 +113,7 @@ def save_nick():
         db.mynick.insert_one(doc)
     else:
         delete_nick = id_list[0]
-        db.usersdb.delete_one(delete_nick)
+        db.mynick.delete_one(delete_nick)
         doc = {
             'cookieId': cookieId_receive,
             'nick': nick_receive
